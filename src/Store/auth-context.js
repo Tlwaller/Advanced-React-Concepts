114f33cc6b9b10198d615dc099ceb00b786bc786
Trey Waller <tlwaller99@gmail.com>
2022-02-08 10:48:39 -0600

    refactoring app with context

--- src/Store/auth-context.js
+++ src/Store/auth-context.js
@@ -1,7 +1,44 @@
-import react from 'react';
+import React, { useState, useEffect } from "react";
 
-const AuthContext = react.createContext({
-    isLoggedIn: false
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  
+  useEffect(() => {
+    const storedUserInfo = localStorage.getItem("isLoggedIn");
+
+    if (storedUserInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
